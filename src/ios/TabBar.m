5fa6097377d2098c47ad3fa7f1f26369484f71ea
Rik Schennink <rikschennink@gmail.com>
2014-02-11 15:07:04 +0100

    - removed webstorm related files - tabbar now shows

--- src/ios/TabBar.m
+++ src/ios/TabBar.m
@@ -12,45 +12,41 @@
 @implementation TabBar
 
 
-/*
 -(CDVPlugin*)initWithWebView:(UIWebView*)theWebView
 {
     self = (TabBar*)[super initWithWebView:theWebView];
     if (self)
 	{
-        tabBarItems = [[NSMutableDictionary alloc] initWithCapacity:5];
-		originalWebViewBounds = theWebView.bounds;
-        tabBarHeight = 49.0f;
-        navBarHeight = 44.0f;
+        
     }
     return self;
 }
-*/
 
 - (void)setup:(CDVInvokedUrlCommand *)command {
     
-    UITabBar *tabBar = [UITabBar new];
+    tabBar = [UITabBar new];
     [tabBar sizeToFit];
     tabBar.delegate = self;
     tabBar.multipleTouchEnabled = NO;
     tabBar.userInteractionEnabled = YES;
 	tabBar.opaque = YES;
     
-    CGFloat height = 49.0f;
+    // setup bounds for tabbar
+    float height = 49.0f;
     CGRect webViewBounds = self.webView.bounds;
-    webViewBounds.size.height += height;
-    CGFloat topBar = 44.0f;
+    
     CGRect tabBarBounds = CGRectMake(
         webViewBounds.origin.x,
-        webViewBounds.origin.y + webViewBounds.size.height - height + topBar,
+        webViewBounds.origin.y + webViewBounds.size.height - height,
         webViewBounds.size.width,
         height
     );
     [tabBar setFrame:tabBarBounds];
     
+    // webview resizes subviews automatically
 	self.webView.superview.autoresizesSubviews = YES;
     
-	[ self.webView.superview addSubview:tabBar];
+	[self.webView.superview addSubview:tabBar];
 }
 
 @end
